95a594ca3cd09d4c4de75921bd3ae9c17c11b869
DilanBernal <dilanbernal01@gmail.com>
2025-09-14 11:58:12 -0500

    Remove unused imports from mainRouter.ts

--- Backend/src/infrastructure/router/mainRouter.ts
+++ Backend/src/infrastructure/router/mainRouter.ts
@@ -1,12 +1,8 @@
 import express, { Request, Router } from "express";
 import userRoutes from "./UserRoutes";
 import friendshipRouter from "./FriendshipRouter";
-import multer from "multer";
 import path from "path";
-import FileAdapter from "../adapter/utils/FileAdapter";
 import fileRouter from "./FileRouter";
-
-// ⬇️ importa tu router de canciones
 import songsRouter from "./songs.routes";
 
 const mainRouter = Router();
@@ -17,7 +13,6 @@ mainRouter.use("/uploads", express.static(path.join(__dirname, "uploads")));
 mainRouter.use("/users", userRoutes);
 mainRouter.use("/friendships", friendshipRouter);
 mainRouter.use("/file", fileRouter);
-
 // ⬇️ monta las rutas de canciones en /api/songs
 mainRouter.use("/songs", songsRouter);
 
